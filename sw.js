4d01a5d8ca8d304338ea6a2837173cdba8fec417
MaksimOS123 <maksim_lebedkov@list.ru>
2018-07-23 22:01:05 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ function networkOrCache(request) {
 
 const FALLBACK =
     '<script>console.log(\"Блин. Чувак. У тебя инета нет((\");</script>\n' + 
-    '<link rel="stylesheet" type="text/css" href="/testSW/css/style.css">\n' +
+    '<link rel="stylesheet" type="text/css" href="/MaksOn/css/style.css">\n' +
     '<div id=\"off\">\n' +
     '    <h1>Хей. Молодец!)<h1>\n' +
     '    <h3>Тебе наверно интересно как это все работает, да?) Что же. Я дам тебе исходники))<h3><p><p>\n' +
@@ -40,8 +40,8 @@ const Error404 =
       '<html\n' +
       '	<head>\n' +
       '	 <title>404. Страница не найдена</title>\n' +
-      '	 <link rel=\"stylesheet\" type=\"text/css\" href=\"/testSW/css/error.css\">\n' +
-      '  <link rel=\"stylesheet\" type=\"text/css\" href=\"/testSW/css/style.css\">\n' +
+      '	 <link rel=\"stylesheet\" type=\"text/css\" href=\"/MaksOn/css/error.css\">\n' +
+      '  <link rel=\"stylesheet\" type=\"text/css\" href=\"/MaksOn/css/style.css\">\n' +
       '  <meta http-equiv=\"content-type\" content=\"text/html\" charset=\"utf-8\">\n' +
       '  <script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n' +
       ' </head>\n' +
@@ -57,7 +57,7 @@ const Error404 =
       '      <blockquote><blockquote></blockquote></blockquote>\n' +
       '     </th><th>\n' +
       '      <h2><b>Страница не найдена</b></h2><p><blockquote><h5>Неправильно набран адрес, или такой страницы больше не существует, а возможно, никогда<br>и не существовало.</h5></blockquote><blockquote><h5><b>Проверьте адрес</b> или <a href="/MaksOn/">перейдите на главную страницу</a>.</h5></blockquote>' +
-      '	    </th><th><img src="/testSW/image/error404.jpg" align=top></th></td></table></html>';
+      '	    </th><th><img src="/MaksOn/image/error404.jpg" align=top></th></td></table></html>';
 
 function useFallback() {
     if (!navigator.onLine) {
