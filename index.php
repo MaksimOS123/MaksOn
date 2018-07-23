04dbbc16a2f0b574776be46d1af480c525c2c274
MaksimOS123 <maksim_lebedkov@list.ru>
2018-07-23 17:46:47 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+﻿<?php
+    require_once("header.php");
+?>
+
+<body>
+	<div id="content">
+		<form action="news" method="post" name="news">
+			<fieldset class="clearfix">
+				<img src="/image/fire.jpg"><br>
+			</fieldset>
+		</form>
+	</div>
+</body>
+
+<?php
+    //Подключение подвала
+    require_once("footer.php");
+?>
